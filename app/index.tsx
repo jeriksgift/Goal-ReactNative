f5c6e18127366d856afec656d6eafddba627e50c
jeriksgift <jeriksgift@gmail.com>
2026-03-19 09:46:34 +0530

    Last at tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,63 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
+  const [goalText, setGoalText] = useState("");
+  const [goals, setGoals] = useState<string[]>([]);
+
+  function goalInputHandler(text: string) {
+    setGoalText(text);
+  }
+
+  function goalAddHandler() {
+    setGoals((currentGoals: string[]) => [...currentGoals, goalText]);
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Enter goal" style={styles.input} onChangeText={goalInputHandler} />
+        <Button title="+" onPress={goalAddHandler} />
+      </View>
+      <View>
+        <Text>List of goals</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#000000ff',
+    paddingBottom: 10,
+    width: '100%',
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#000000ff',
+    padding: 10,
+    marginRight: 10,
+  },
+  primary_btn: {
+    margin: 20,
+    padding: 10,
+    borderRadius: 20,
+    fontSize: 50,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000ff",
+    color: "#ffffffff"
+  }
+});
